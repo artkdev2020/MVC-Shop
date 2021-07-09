1716071132556830ce79e67791577ef15feb2ec2
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-07-09 10:23:21 +0300

    index with carousel and dbrepository

--- OnlineShop/OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/OnlineShop/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
 using OnlineShop.Models;
-using OnlineShop.Models.SubModels;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,31 +10,13 @@ namespace OnlineShop.Controllers
 {
     public class HomeController : Controller
     {
-        //public ApplicationDbContext _context { get; }
-        //public IndexModel _header { get; }
-
-        //public HomeController(ApplicationDbContext context)
-        //{
-        //    //_context = context;
-        //    //_header = new IndexModel(context.Categories.ToList(), context.Subcategories.ToList());
-        //}
-
         public IActionResult Index()
         {
-            //IndexModel header = new IndexModel(context.Categories.ToList(),
-            //    context.Subcategories.ToList());
-            //ProductWithImage carouselProducts = new ProductWithImage(_context.Products.ToList(),
-            //    _context.ProductImages.ToList());
-
-            //ViewData["Header"] = _header;
-            //ViewData["CarouselProducts"] = carouselProducts;
             return View();
         }
 
         public async Task<IActionResult> Privacy()
         {
-            //ViewData["Header"] = _header;
-            //return View(await _context.Subcategories.ToListAsync());
             return View();
 
         }
--- OnlineShop/OnlineShop/Services/DbRepository.cs
+++ OnlineShop/OnlineShop/Services/DbRepository.cs
@@ -1,14 +1,31 @@
 ﻿using OnlineShop.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OnlineShop.Services
 {
     public class DbRepository
     {
-        public ApplicationDbContext Context { get; set; }
+        private ApplicationDbContext _context;
 
         public DbRepository(ApplicationDbContext context)
         {
-            Context = context;
+            _context = context;
+        }
+
+        public IEnumerable<OnlineShop.Models.Category> Categories
+        {
+            get { return _context.Categories.ToList(); }
+        }
+
+        public IEnumerable<OnlineShop.Models.Subcategory> Subcategories
+        {
+            get { return _context.Subcategories.ToList(); }
+        }
+
+        public IEnumerable<OnlineShop.Models.Product> Products
+        {
+            get { return _context.Products.ToList(); }
         }
     }
 
